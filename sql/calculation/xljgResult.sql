5e988cb2f8943333dfb518ccff1cb9a546da7ccd
Beier <wber0712@hotmail.com>
2017-12-19 21:38:59 +0800

    修改计算错误。

--- sql/calculation/xljgResult.sql
+++ sql/calculation/xljgResult.sql
@@ -1,7 +1,7 @@
 select
 REFINE_GJ,REFINE_BJ,REFINE_BS,zz_bj,szgj,
-((zz_bs*zz_bj)/100+(100-zz_bj))*szgj/100*skill/100
-,szgj*zz_bs/100*skill/100
+szgj*((zz_bs*zz_bj)+(100-zz_bj)*100)*skill/1000000
+,szgj*zz_bs*skill/10000
 from (select REFINE_GJ,REFINE_BJ,REFINE_BS,skill,bs,
 REFINE_GJ*20+100+gj zz_gj,min(REFINE_BJ*180+SL_BJ,1000)/10 zz_bj,(REFINE_BS*500+bs+SL_BS)/10 zz_bs
 ,(REFINE_GJ*20+100)*MAX_GJ/100 szgj
